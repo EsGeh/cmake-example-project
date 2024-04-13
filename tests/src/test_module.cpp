7cae7854f455460ca010b1fdb4dbe6b9fae9c286
EsGeh <samuel@esgeh.net>
2024-04-13 13:01:40 +0200

    upgrade catch2 -> v3

--- tests/src/test_module.cpp
+++ tests/src/test_module.cpp
@@ -1,10 +1,9 @@
-#define CATCH_CONFIG_MAIN
-
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <module.h>
 
 TEST_CASE( "test 'function'", "[main]" ) {
 	double arg = 9;
-	REQUIRE( function( arg ) == Approx( 3 ) );
+	REQUIRE_THAT( function( arg ), Catch::Matchers::WithinAbs( 3, 0.001) );
 }
 
